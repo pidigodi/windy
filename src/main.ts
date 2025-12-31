4164886c0be752ce4b1cfa594c463b7400c2caef
John Bohl <johnbohl@outlook.com>
2026-01-01 12:42:47 +1300

    first

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
